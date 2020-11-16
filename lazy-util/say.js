4adff6692972108b2d0ff3a772abd6769257b904
MaRuiKeSi <947448592@qq.com>
2020-11-16 17:01:53 +0800

    提交时间: 2020-11-16 17:1:53 -- 模板编译部分 几个函数支架架起来了  继续加油继续加油!!!

--- lazy-util/say.js
+++ lazy-util/say.js
@@ -5,3 +5,9 @@ let iSay = '模板编译部分 几个函数支架架起来了  继续加油继�
 module.exports = {
   iSay
 }
+
+
+/**
+ *   1. diff Dom算法   更新子节点  优化更新子节点
+ *   2. 模板字符串(html字符串)编译  ast生成
+ */
--- src/compiler/parser/index.js
+++ src/compiler/parser/index.js
@@ -7,8 +7,45 @@ export function parse(template, options) {
     //...
     test() {
       console.log('test test test 钩子函数')
+    },
+    start(tag, attrs, unary, start, end) {
+      let currentParent = null
+      let element = createASTElement(tag, attrs, currentParent)
+    },
+    end(tag, start, end) {
+      console.log('end钩子')
+    },
+    chars(text, start, end) {
+      console.log('chars钩子')
+    },
+    comment(text) {
+      let element = {
+        type: 3,
+        text,
+        isComment: true
+      }
     }
+
   })
   root = 'root ast ast ast ast'
   return root
+}
+
+export function createASTElement(tag, attrs, parent) {
+  return {
+    type: 1,
+    tag,
+    attrsList: attrs,
+    attrsMap: makeAttrsMap(attrs),
+    parent,
+    children: []
+  }
+}
+
+function makeAttrsMap(attrs) {
+  const map = {}
+  for (let i = 0, l = attrs.length; i < l; i++) {
+    map[attrs[i].name] = attrs[i].value
+  }
+  return map
 }
